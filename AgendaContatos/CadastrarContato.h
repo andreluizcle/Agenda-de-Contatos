25ae4efe62cad4f6895e80f28c7c0b115a6d3474
Arthur Nero <nerooliveira.arthur@gmail.com>
2025-10-30 17:43:37 -0300

    Ajustes na inclusão de arquivos externos
    Ajustes na inclusão de arquivos externos devido a alteração de locais de arquivos no projeto

--- AgendaContatos/CadastrarContato.h
+++ AgendaContatos/CadastrarContato.h
@@ -2,8 +2,8 @@
 #define CADASTRARCONTATO_H
 
 // Inclusão de arquivos externos
-#include "../EstruturaContato.h"
-#include "../LerArquivo.h" // -> Ele é incluído pois tem o nome do arquivo definido
+#include "EstruturaContato.h"
+#include "LerArquivo.h" // -> Ele é incluído pois tem o nome do arquivo definido
 
 //Headers dos métodos para cadastro
 void CadastrarContato(FILE* arquivo, Contato novoContato);
