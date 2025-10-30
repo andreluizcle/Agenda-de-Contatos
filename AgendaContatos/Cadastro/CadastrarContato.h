43350a4750dc9860e06b09f3c8481089d2d5dbca
Arthur Nero <nerooliveira.arthur@gmail.com>
2025-10-29 22:11:16 -0300

    Atualização nas assinaturas das funções
    Pequeno ajuste nas assinaturas das funções de Cadastro

--- AgendaContatos/Cadastro/CadastrarContato.h
+++ AgendaContatos/Cadastro/CadastrarContato.h
@@ -6,7 +6,7 @@
 #include "../LerArquivo.h" // -> Ele é incluído pois tem o nome do arquivo definido
 
 //Headers dos métodos para cadastro
-void CadastrarNovoContato(FILE* arquivo, Contato novoContato);
-void CadastrarNovoContatoArquivo(Contato novoContato);
+void CadastrarContato(FILE* arquivo, Contato novoContato);
+void CadastrarContatoArquivo(Contato novoContato);
 
 #endif
