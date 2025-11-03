8b7e8ef322aaf4a60c5d3700d2d46ecd0bc74134
Arthur Nero <nerooliveira.arthur@gmail.com>
2025-11-02 23:45:15 -0300

    Finalização da 3ª versão do Cadastro
    Agora isso tá funcionando sem inconsistências.

--- AgendaContatos/CadastrarContato.c
+++ AgendaContatos/CadastrarContato.c
@@ -1,6 +1,7 @@
 // Inclusão de bibliotecas
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 // Inclusão de arquivos externos (estruturas de funções, detalhes fundamentais etc)
 #include "EstruturaContato.h"
@@ -8,29 +9,27 @@
 #include "CadastrarContato.h"
 
 // Método de cadastro do contato no arquivo
-/* 
-    A ideia do método era encontrar a posição alfabética em que o novo contato
-    deveria ser inserido e inserí-lo, porém isso tornou-se inviável por ser
-    possível apenas com a criação de um arquivo temporário e um processo exten-
-    so demais para ser descrito aqui.
-    Portanto a função é implementada com a sobrescrita de um contato excluído
-    ou com o registro normal (no fim do .txt) ;]
-*/
 void CadastrarContato(FILE* arquivo, Contato novoContato){
     Contato contato;
     int cadastrado = 0;
+    // long é usado para trabalhar com valores em bytes (ponteiro de um arquivo, por exemplo)
     long posicaoAtual = 0;
 
     while(fscanf(arquivo, "%d|%[^|]|%[^|]|%[^|]|%[^|]|%d\n", &contato.codigo, contato.nome, contato.telefone, contato.email, contato.endereco, &contato.ativo) != EOF){
-        if(!(contato.ativo && cadastrado)){
+        if(!(contato.ativo) && !(cadastrado)){
+            // Move o ponteiro (em bytes) para o começo da linha em que ativo == 0
             fseek(arquivo, posicaoAtual, SEEK_SET);
+            
             fprintf(arquivo, "%d|%s|%s|%s|%s|%d\n", novoContato.codigo, novoContato.nome, novoContato.telefone, novoContato.email, novoContato.endereco, novoContato.ativo);
             cadastrado = 1;
             break;
         }
+        // Recebe a posição atual do ponteiro - vem depois do fprintf para que o ponteiro sempre esteja no começo da linha
         posicaoAtual = ftell(arquivo);
     }
 
+    // Se não houver nenhum contato que foi excluído, não entrará na primeira condicional
+    // necessitando, então, dessa
     if(!cadastrado)
         fprintf(arquivo, "%d|%s|%s|%s|%s|%d\n", novoContato.codigo, novoContato.nome, novoContato.telefone, novoContato.email, novoContato.endereco, novoContato.ativo);
 }
@@ -39,6 +38,20 @@ void CadastrarContato(FILE* arquivo, Contato novoContato){
 void CadastrarContatoArquivo(Contato novoContato){
     // Definição de valor base para o campo de controle de existência de contato
     novoContato.ativo = 1;
+
+    // Preenchendo todas as strings até o tamanho máximo para que todos os contatos ocupem o mesmo espaço na memória
+    while(strlen(novoContato.nome)<(tamanhoPadrao-1)){
+        strcat(novoContato.nome, " ");
+    }
+    while(strlen(novoContato.telefone)<(tamanhoPadrao-1)){
+        strcat(novoContato.telefone, " ");
+    }
+    while(strlen(novoContato.email)<(tamanhoPadrao-1)){
+        strcat(novoContato.email, " ");
+    }
+    while(strlen(novoContato.endereco)<(tamanhoPadrao-1)){
+        strcat(novoContato.endereco, " ");
+    }
 
     // Acessar o método de cadastro
     void (*Comando)(FILE*, Contato) = CadastrarContato;
