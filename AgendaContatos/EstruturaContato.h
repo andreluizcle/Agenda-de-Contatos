1b5fdf918642fa09e8a789e51c01597750fb6bfa
Arthur Nero <nerooliveira.arthur@gmail.com>
2025-11-02 23:36:19 -0300

    Ajuste na estrutura de Contato
    Uso de constante para controlar o tamanho de campos de texto de Contato

--- AgendaContatos/EstruturaContato.h
+++ AgendaContatos/EstruturaContato.h
@@ -1,13 +1,14 @@
 #ifndef ESTRUTURACONTATO_H
 #define ESTRUTURACONTATO_H
+#define tamanhoPadrao 50
 
 //
 typedef struct {
     int codigo;
-    char nome[50];
-    char telefone[50];
-    char email[50];
-    char endereco[50];
+    char nome[tamanhoPadrao];
+    char telefone[tamanhoPadrao];
+    char email[tamanhoPadrao];
+    char endereco[tamanhoPadrao];
     int ativo;
 } Contato;
 
