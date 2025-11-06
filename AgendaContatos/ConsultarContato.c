1a12c44823760327426a5c728cd26e93a9fbb701
felipekenji-dev <felipejaponeskof@gmail.com>
2025-11-06 04:10:28 -0300

    Implementação Parcial da Consulta de Contato
    Implementação parcial da consulta de contato específico e correções de nomenclatura e nomeação de variáveis.

--- AgendaContatos/ConsultarContato.c
+++ AgendaContatos/ConsultarContato.c
@@ -9,41 +9,43 @@
 #include "Headers/ConsultarContato.h"
 
 // Métodos de Consulta de Contatos
-int ListarContatos(FILE* arquivo, Contato** dados) {
+int ListarContatos(FILE* arquivo, Contato** listaContatos) {
     // Ler Cada Bloco do Arquivo
     int indice = 0;
     Contato contatoLido;
     
     while (fread(&contatoLido, sizeof(Contato), 1, arquivo) > 0) {
-        // Aumentar o Tamanho do Ponteiro para Guardar todos os Contatos
-        *dados = (Contato*)realloc(*dados, (indice+1)*sizeof(Contato));
-        
-        // Guardar os Dados do Ponteiro no Vetor
-        (*dados)[indice].codigo = contatoLido.codigo;
-        strcpy((*dados)[indice].nome, contatoLido.nome);
-        strcpy((*dados)[indice].telefone, contatoLido.telefone);
-        strcpy((*dados)[indice].email, contatoLido.email);
-        strcpy((*dados)[indice].endereco, contatoLido.endereco);
-        (*dados)[indice].ativo = contatoLido.ativo;
-        
-        // Mudar Índice para Próxima Leitura
-        indice++;
+        if (contatoLido.ativo) {
+            // Aumentar o Tamanho do Ponteiro para Guardar todos os Contatos
+            *listaContatos = (Contato*)realloc(*listaContatos, (indice+1)*sizeof(Contato));
+            
+            // Guardar os Dados do Contato Ativo no Vetor
+            (*listaContatos)[indice].codigo = contatoLido.codigo;
+            strcpy((*listaContatos)[indice].nome, contatoLido.nome);
+            strcpy((*listaContatos)[indice].telefone, contatoLido.telefone);
+            strcpy((*listaContatos)[indice].email, contatoLido.email);
+            strcpy((*listaContatos)[indice].endereco, contatoLido.endereco);
+            (*listaContatos)[indice].ativo = contatoLido.ativo;
+            
+            // Mudar Índice para Próxima Leitura
+            indice++;
+        }
     }
     
     // Retornar Quantidade de Contatos
     return indice;
 }
 
-int ListarContatosArquivo(Contato** dados) {
+int ListarContatosArquivo(Contato** listaContatos) {
     // Rodar o Comando no Arquivo
     int (*Comando)(FILE*, Contato**) = ListarContatos;
-    int quantidadeContatos = Consultar(Comando, "rb", dados);
+    int quantidadeContatos = Consultar(Comando, "rb", listaContatos);
 
     // Retornar Quantidade de Contatos
     return quantidadeContatos;
 }
 
-int ConsultarUltimoContato(FILE* arquivo, Contato** dados) {
+int ConsultarUltimoContato(FILE* arquivo, Contato** contato) {
     // Ler Cada Bloco do Arquivo
     int ultimoCodigo = 0;
     Contato contatoLido;
@@ -51,12 +53,12 @@ int ConsultarUltimoContato(FILE* arquivo, Contato** dados) {
     while(fread(&contatoLido, sizeof(Contato), 1, arquivo) > 0) {
         // Guardar os Dados Somente do Último Contato
         if(contatoLido.ativo && contatoLido.codigo > ultimoCodigo) {
-            (*dados)[0].codigo = contatoLido.codigo;
-            strcpy((*dados)[0].nome, contatoLido.nome);
-            strcpy((*dados)[0].telefone, contatoLido.telefone);
-            strcpy((*dados)[0].email, contatoLido.email);
-            strcpy((*dados)[0].endereco, contatoLido.endereco);
-            (*dados)[0].ativo = contatoLido.ativo;
+            (*contato)->codigo = contatoLido.codigo;
+            strcpy((*contato)->nome, contatoLido.nome);
+            strcpy((*contato)->telefone, contatoLido.telefone);
+            strcpy((*contato)->email, contatoLido.email);
+            strcpy((*contato)->endereco, contatoLido.endereco);
+            (*contato)->ativo = contatoLido.ativo;
         }
     }
 
@@ -64,11 +66,41 @@ int ConsultarUltimoContato(FILE* arquivo, Contato** dados) {
     return 1;
 }
 
-int ConsultarUltimoContatoArquivo(Contato** dados) {
+int ConsultarUltimoContatoArquivo(Contato** contato) {
     // Rodar o Comando no Arquivo
     int (*Comando)(FILE*, Contato**) = ConsultarUltimoContato;
-    int ultimoCodigo = Consultar(Comando, "rb", dados);
+    int ultimoCodigo = Consultar(Comando, "rb", contato);
 
     // Retornar Quantidade de Contatos
     return ultimoCodigo;
 }
+
+// int ConsultarContatoSelecionado(FILE* arquivo, Contato** contato) {
+//     // Ler Cada Bloco do Arquivo
+//     int codigoContato = (*contato)[0].codigo;
+//     Contato contatoLido;
+
+//     while(fread(&contatoLido, sizeof(Contato), 1, arquivo) > 0) {
+//         // Guardar os Dados Somente do Contato Escolhido
+//         if(contatoLido.ativo && contatoLido.codigo > ultimoCodigo) {
+//             (*contato)->codigo = contatoLido.codigo;
+//             strcpy((*contato)->nome, contatoLido.nome);
+//             strcpy((*contato)->telefone, contatoLido.telefone);
+//             strcpy((*contato)->email, contatoLido.email);
+//             strcpy((*contato)->endereco, contatoLido.endereco);
+//             (*contato)->ativo = contatoLido.ativo;
+//         }
+//     }
+
+//     // Retornar Quantidade de Contatos
+//     return 1;
+// }
+
+// int ConsultarContatoSelecionadoArquivo(Contato** contato) {
+//     // Rodar o Comando no Arquivo
+//     int (*Comando)(FILE*, Contato**) = ConsultarContatoSelecionado;
+//     int ultimoCodigo = Consultar(Comando, "rb", contato);
+
+//     // Retornar Quantidade de Contatos
+//     return ultimoCodigo;
+// }
