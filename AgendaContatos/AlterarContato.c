4e31d44090d51edfaa8b5958f78eaab705e5cdb6
gsoaresslv <guilhermesoaresvixbr@gmail.com>
2025-11-11 20:22:03 -0300

    Adição do Recurso de Alteração de Contato

---
+++ AgendaContatos/AlterarContato.c
@@ -0,0 +1,31 @@
+// Inclusão de bibliotecas
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+// Inclusão de arquivos externos (estruturas de funções, detalhes fundamentais etc)
+#include "Headers/EstruturaContato.h"
+#include "Headers/LerArquivo.h"
+#include "Headers/CadastrarContato.h"
+#include "Headers/ConsultarContato.h"
+
+void AlterarContato(FILE* arquivo, Contato contatoalt) {
+    Contato contato;
+
+    // Alterando o contato no arquivo
+    fseek(arquivo, 0, SEEK_SET); // Retorna ao início do arquivo
+      while(fread(&contato, sizeof(Contato), 1, arquivo) == 1){
+        if(contato.codigo == contatoalt.codigo) {
+            contato.codigo = contatoalt.codigo;
+            contato.ativo = contatoalt.ativo;
+            contato.nome[0] = contatoalt.nome[0];
+            contato.telefone[0] = contatoalt.telefone[0];
+            contato.email[0] = contatoalt.email[0];
+            contato.endereco[0] = contatoalt.endereco[0];
+            // Move o ponteiro (em bytes) para o começo da linha em que id == contatoalt.id p/ sobrescrita
+            fseek(arquivo, -(long)sizeof(Contato), SEEK_CUR);
+            fwrite(&contato, sizeof(Contato), 1, arquivo);
+            break;
+        }
+    }
+}
